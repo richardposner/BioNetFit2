673f081778398193c05270a3923f0edb8b9bf7e2
ionif <aionkov@gmail.com>
2017-06-22 09:48:32 -0600

    removed sh files for compilation of single files and pOUT which is output from runs

--- Pheromones.sh
+++
@@ -1,6 +0,0 @@
-echo "This is a shell script to compile only one file"  
-cd bin
-rm -f src/code/Pheromones.o src/code/Pheromones.d
-mpic++ -D__GXX_EXPERIMENTAL_CXX0X__ -D__cplusplus=201103L -I../include -I/usr/lib/openmpi/include -O3 -g3 -Wall -c -fmessage-length=0 -std=gnu++0x -MMD -MP -MF"src/code/Pheromones.d" -MT"src/code/Pheromones.d" -o "src/code/Pheromones.o" "../src/code/Pheromones.cpp"
-cd ..
-echo "Operation is finished"  
--- genfit.sh
+++
@@ -1,6 +0,0 @@
-echo "This is a shell script to compile only one file"  
-cd bin
-rm -f GenFit2.o GenFit2.d
-mpic++ -fpermissive -D__GXX_EXPERIMENTAL_CXX0X__ -D__cplusplus=201103L -I../include -I/usr/lib/openmpi/include -O3 -g3 -Wall -c -fmessage-length=0 -std=gnu++0x -MMD -MP -MF"src/GenFit2.d" -MT"src/GenFit2.d" -o "src/GenFit2.o" "../src/GenFit2.cpp"
-cd ..
-echo "Operation is finished"  
